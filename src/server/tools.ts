5a7137492a036d8992e54bca445ce6896c45b237
wangxunxun <wangxunxun3094@qq.com>
2025-08-05 22:59:23 +0800

    5

--- src/server/tools.ts
+++ src/server/tools.ts
@@ -1,24 +1,34 @@
+import { ParameterizedContext } from 'koa'
 import Router from 'koa-router'
 import { createServer } from '../mcp/tools'
 
 const router = new Router({ prefix: '/tools' })
 const server: any = createServer()
 
-if (router && server && (server as any).tools) {
-  server.tools.forEach((tool: any, toolName: string) => {
-    router.get(`/${toolName}`, async (ctx: any) => {
-      try {
-        if (typeof tool.call === 'function') {
-          const result = await tool.call(server, ctx.query)
-          ctx.body = result
-        } else {
-          ctx.body = { error: 'Tool does not have a callable interface.' }
-        }
-      } catch (err: any) {
-        ctx.body = { error: err?.message || 'Tool execution error.' }
-      }
-    })
-  })
-}
+router.get('/', async (ctx: ParameterizedContext) => {
+  const { toolName, ...args } = ctx?.query as any
+  try {
+    if (toolName) {
+      const tools: any = server._registeredTools
+      const params = Object.keys(args).reduce((acc: any, item: any) => {
+        acc[item] = JSON.parse(args[item])
+        return acc
+      }, {})
+      const result = await tools[toolName].callback(params)
+      ctx.body = { code: 0, data: result, msg: 'success' }
+    } else {
+      const result = Object.entries(server._registeredTools).reduce(
+        (acc: any, item: any) => {
+          acc[item[0]] = item[1].description
+          return acc
+        },
+        {},
+      )
+      ctx.body = { code: 0, data: result, msg: 'success' }
+    }
+  } catch (error: any) {
+    ctx.body = { code: -1, data: [], msg: error.message }
+  }
+})
 
 export default router
