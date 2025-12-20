ade2dc36120525cd71bc0f3e5eddf3bb1bb46c17
wangxunxun <wangxunxun3094@qq.com>
2025-12-20 22:16:54 +0800

    fix: 合并接口服务

---
+++ api/nhsa.ts
@@ -0,0 +1,25 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+export default (req: any, res: any) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With',
+  )
+
+  // 如果是OPTIONS请求，直接返回200
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  const target = 'https://wx.nhsa.gov.cn' // 你的后端API地址
+  createProxyMiddleware({
+    target: target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/nhsa': '/nhsa', // 重写路径
+    },
+  })(req, res)
+}
