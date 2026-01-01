f15466473c9dff583adc07aa0c6fbb1b533533f8
wangxunxun <wangxunxun3094@qq.com>
2026-01-01 22:04:20 +0800

    修改dailyhot-api版本

--- api/dailyhot.ts
+++ api/dailyhot.ts
@@ -1,7 +1,3 @@
-// @ts-ignore - dailyhot-api doesn't export app directly, but we can access it
 import serveHotApi from 'dailyhot-api'
-import app from 'dailyhot-api/dist/app.js'
 
-export default async (req: Request) => {
-  return app.fetch(req)
-}
+export default serveHotApi()
